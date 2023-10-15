d1ea6a7d9682b109dbaf0bf7659b0fd64d15d982
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-15 15:51:11 -0700

    start of exchange_message

--- diffie-hellman.py
+++ diffie-hellman.py
@@ -10,12 +10,41 @@ from Crypto.Hash import SHA256
 
 BLOCK_SIZE = 16         # 128 bits
 
-def calc_key(p,g) :      #p and g comes from sender
-    secret_value = randint(1, p-2)
-    val = g**(secret_value)
+def calc_key(p,g, a) :      #p and g comes from sender
+    val = g**(a)
     return val % p
 
-def key_exchange(A,a,p) : #A is calculated by opposite party
-    key = (A**a)%p
-    h = SHA256.new()
-    h.update(key)
+def key_exchange(their_value,my_int,p) : #A is calculated by opposite party
+    shared_key = bytes((their_value**my_int)%p)
+    h = SHA256.new(data = shared_key)
+    return h.hexdigest()
+
+def create_cipher(message, key, iv):
+
+    cipher = AES.new(key.encode("utf8"), AES.MODE_CBC, iv)
+    cipher_text = cipher.encrypt(message)
+    return cipher_text
+
+def exchange_message(message, key, iv): #takes in the encrypted message
+    cipher = AES.new(key.encode("utf8"), AES.MODE_CBC, iv)
+    decrypted = cipher.decrypt(message)
+    return decrypted
+if __name__ == "__main__":
+    p = 37
+    g = 5
+    iv = get_random_bytes(16)
+    alice_a = randint(1, p-2)
+    alice = calc_key(p, g, alice_a)
+    bob_b = randint(1, p-2)
+    bob = calc_key(p, g, bob_b)
+
+    alice_key = bin(int(key_exchange(bob, alice_a, p), 16))[2:18]
+    bob_key = bin(int(key_exchange(alice, bob_b, p),16))[2:18]
+ 
+
+    print("Alice's cipher:", create_cipher(pad(b"hi bob", 16), str(alice_key), iv))
+    print("Bob's cipher:", create_cipher(pad(b"hi alice", 16), str(bob_key), iv))
+
+    a_msg = create_cipher(pad(b"hi bob", 16), str(alice_key), iv)
+    b_msg = create_cipher(pad(b"hi alice", 16), str(bob_key), iv)
+    print("Bob's decrypted message: ", exchange_message(unpad(b_msg,16), str(alice_key), iv))
