e27e15b77792f6c3cbd3a28105153820e60d8614
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-15 14:09:08 -0700

    start task1

--- diffie-hellman.py
+++ diffie-hellman.py
@@ -5,5 +5,17 @@
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from random import randint
+from Crypto.Hash import SHA256
 
 BLOCK_SIZE = 16         # 128 bits
+
+def calc_key(p,g) :      #p and g comes from sender
+    secret_value = randint(1, p-2)
+    val = g**(secret_value)
+    return val % p
+
+def key_exchange(A,a,p) : #A is calculated by opposite party
+    key = (A**a)%p
+    h = SHA256.new()
+    h.update(key)
