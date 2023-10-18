22a0216162440319682df1984cd26798b9199aa4
joanna-chou <joanna.chouu@gmail.com>
2023-10-18 13:32:17 -0700

    Fixed padding and casting issues

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -9,18 +9,17 @@ from Crypto.Hash import SHA256
 
 BLOCK_SIZE = 16      # 128 bits
 
-
 def formatHex(input: hex) :
     input = input.replace(" ", "")
     input = int(input, 16)
     return input
         
-def calc_key(p: int,g: int, a:int) -> int :      #p and g comes from sender
-    val = g**(a)
-    return val % p
+def calc_key(p: int,g: int, a:int) -> int :      #p and g comes from sender 
+    value =  pow(g,a,p)
+    return value
 
 def key_exchange(their_value: int, my_int: int, p:int) -> hex: #A is calculated by opposite party
-    shared_key = bytes((their_value**my_int)%p)
+    shared_key = bytes(str(pow(their_value, my_int, p)).encode("utf8"))         # int -> str -> bytes
     h = SHA256.new(data = shared_key)
     return h.hexdigest()
 
@@ -36,10 +35,7 @@ def exchange_message(message: bytes, key:str, iv: bytes) -> bytes: #takes in the
     return decrypted
 
 if __name__ == "__main__":
-    p = 37
-    g = p-1
 
-    # HUHHHH p and g                        ????
     p = formatHex("B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 DF1FB2BC 2E4A4371")
     g = formatHex("A4D1CBD5 C3FD3412 6765A442 EFB99905 F8104DD2 58AC507F D6406CFF 14266D31 266FEA1E 5C41564B 777E690F 5504F213 160217B4 B01B886A 5E91547F 9E2749F4 D7FBD7D3 B9A92EE1 909D0D22 63F80A76 A6A24C08 7A091F53 1DBF0A01 69B6A28A D662A4D1 8E73AFA3 2D779D59 18D08BC8 858F4DCE F97C2A24 855E6EEB 22B3B2E5")
 
--- minm_attack.py
+++ minm_attack.py
@@ -13,8 +13,8 @@ def minm_attack_on_keys():
     alice_key = mallory_key
     bob_key = mallory_key
 
-    alice_cipher = create_cipher(pad(b"hi bob", 16), str(alice_key), iv)
-    bob_cipher = create_cipher(pad(b"hi alice", 16), str(bob_key), iv)
+    alice_cipher = create_cipher(pad(b"hi bob", BLOCK_SIZE), str(alice_key), iv)
+    bob_cipher = create_cipher(pad(b"hi alice", BLOCK_SIZE), str(bob_key), iv)
 
     print("Alice's cipher:", alice_cipher)
     print("Bob's cipher:", bob_cipher)
@@ -34,25 +34,27 @@ def minm_attack_on_g(p, g):
     alice_key = bin(int(key_exchange(bob, alice_a, p), 16))[2:18]
     bob_key = bin(int(key_exchange(alice, bob_b, p),16))[2:18]
 
-    alice_cipher = create_cipher(pad(b"hi bob", 16), str(alice_key), iv)
-    bob_cipher = create_cipher(pad(b"hi alice", 16), str(bob_key), iv)
+    alice_cipher = create_cipher(pad(b"hi bob", BLOCK_SIZE), str(alice_key), iv)
+    bob_cipher = create_cipher(pad(b"hi alice", BLOCK_SIZE), str(bob_key), iv)
 
     print("Alice's cipher:", alice_cipher)
     print("Bob's cipher:", bob_cipher)
     print("Bob's decrypted message: ", exchange_message(bob_cipher, str(bob_key), iv))
     print("Alice's decrypted message: ", exchange_message(alice_cipher, str(alice_key), iv))
 
-    #tampering with g (Task 2B)
     if p == g:
-        h = SHA256.new(data = bytes(0))
+        h = SHA256.new(data = bytes(str(0).encode("utf8")))
     elif g == 1:
-        h = SHA256.new(data = bytes(1))
+        h = SHA256.new(data = bytes(str(1).encode("utf8")))
     elif g == p-1:
         if (alice_a % 2 == 0):
-            h = SHA256.new(data = bytes(1))
+            h = SHA256.new(data = bytes(str(1).encode("utf8")))
         else:
-            h = SHA256.new(data = bytes(p-1))
+            h = SHA256.new(data = bytes(str(p-1).encode("utf8")))
     mallory_key = bin(int(h.hexdigest(),16))[2:18]
+    print("mallory's key", mallory_key)
+    print("alice's key", alice_key)
+    print("bob's key", bob_key)
     print("Mallory's decrypted message (from Alice): ", exchange_message(alice_cipher, str(mallory_key), iv))
     print("Mallory's decrypted message (from Bob): ", exchange_message(bob_cipher, str(mallory_key), iv))
 
@@ -61,12 +63,15 @@ def minm_attack_on_g(p, g):
 if __name__ == "__main__":
     print("\nTASK 2A: key tampering ------------------------------------------------------------------------")
     minm_attack_on_keys()
+    p = formatHex("B10B8F96 A080E01D DE92DE5E AE5D54EC 52C99FBC FB06A3C6 9A6A9DCA 52D23B61 6073E286 75A23D18 9838EF1E 2EE652C0 13ECB4AE A9061123 24975C3C D49B83BF ACCBDD7D 90C4BD70 98488E9C 219A7372 4EFFD6FA E5644738 FAA31A4F F55BCCC0 A151AF5F 0DC8B4BD 45BF37DF 365C1A65 E68CFDA7 6D4DA708 DF1FB2BC 2E4A4371")
 
     print("\nTASK 2B: g = p -----------------------------------------------------------------------------")
-    minm_attack_on_g(p=37, g=37)
+    g = p
+    minm_attack_on_g(p, g)
     print("\nTASK 2B: g = 1 -----------------------------------------------------------------------------")
-    minm_attack_on_g(p=37, g=1)
+    minm_attack_on_g(p, g=1)
     print("\nTASK 2B: g = p-1 ---------------------------------------------------------------------------")
-    minm_attack_on_g(p=37, g=36)
+    g = p - 1
+    minm_attack_on_g(p, g)
 
     
