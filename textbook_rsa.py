4a31022c38736eee66c3c3fcc65e96507fcd1739
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-19 22:02:18 -0700

    finished task 3 part 1

--- textbook_rsa.py
+++ textbook_rsa.py
@@ -1,6 +1,10 @@
 # Task 3
 from Crypto.Util.number import getPrime
 import random
+import math
+from binascii import hexlify
+from minm_attack import *
+
 
 #calculates the inverse of a number mod n
 def inverse(a,n) :
@@ -10,31 +14,62 @@ def inverse(a,n) :
    new_r = a
    while (new_r != 0) :
        quotient = r/new_r
-       (t,new_t) = (new_t, t - quotient * new_t)
-       (r, new_r) = (new_r, r - quotient * new_r)
+       (t,new_t) = (new_t, t - (quotient * new_t))
+       (r, new_r) = (new_r, r - (quotient * new_r))
    if (r > 1) :
          return "a is not invertible"
    if (t < 0) :
       t = t + n
    return t
 
+def RSA_encrypt(m,e,n):
+   return pow(m,e,n)
 
+def RSA_decrypt(c,d,n):
+    return pow(c,d,n)
 
-    
+def textbook_rsa():
+   e = 65537                #2^16 + 1
+   #Alice public key
+   p = getPrime(512)
+   q = getPrime(512)   
+   n = p * q
+   phi = (p-1)*(q-1)
+   m =  "hello angelika"
+   print("original msg:", m)
+   m = int(hexlify(b"hello angelika"), 16)
+   encrypted_msg = RSA_encrypt(m,e,n)
+   #print("encoded msg:", encrypted_msg)
+   d = pow(e, -1, phi)
+   #fake_d = (e**-1) % phi          # WHY ARE THESE DIFFERENT???
+   #print("d:", d)
+
+   decrypted_msg = hex(RSA_decrypt(encrypted_msg,d,n))[2:]
+   msg = bytes.fromhex(decrypted_msg).decode('utf-8')
+   print("Decoded msg: ", msg)
+
+def textbook_rsa_attack():
+   e = 65537
+   p = getPrime(512)
+   q = getPrime(512)   
+   n = p * q
+   phi = (p-1)*(q-1)
+   bob_s = 13
+   c = RSA_encrypt(bob_s,e,n)
+   #print("bob's original cipher to Alice", c)
+   # MALLORY DOES fishy shit
+   # f(c) mallebility attack
+   mallory_m = 333
+   print("mallory's mallebility attack:", mallory_m)
+
+   c = pow((2*mallory_m), e, n)           # why does this work when u dont multiply by 2 but bruce's notes say to???
+   
+   d = pow(e, -1, phi)
+   alice_s = RSA_decrypt(c, d, n)
+   print("decrypted tampered message:", alice_s)
+   
 
 if __name__ == "__main__":
-    #Alice public key
-    alice_p = getPrime(512)
-    alice_q = getPrime(512)
-    alice_n = alice_p * alice_q
-    alice_theta = (alice_p-1)*(alice_q-1)
-
-   #Bob public key
-    bob_p = getPrime(512)
-    bob_q = getPrime(512)
-    bob_n = bob_p * bob_q
-    bob_theta = (bob_p-1)*(bob_q-1)
-
-
-
-
+   textbook_rsa()
+   print("-----------------------------------------------------------------------------")
+   textbook_rsa_attack()
