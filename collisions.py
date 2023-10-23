88f199225a5dec7491ea138be94c7bd77cdcdc87
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-23 16:35:25 -0700

    finds collision using birthday problem

--- collisions.py
+++ collisions.py
@@ -5,9 +5,31 @@ from Crypto.Hash import SHA256
 
 def collisions(input: bytes):
     input = input[:8]
-    print("Truncated input: ", input)
-    hash = SHA256.new(bytes(input.encode("utf8")))
-    print("Hashed input in hex:", hash.hexdigest())
+    return input
+    #print("Truncated input: ", input)
+    # hash = SHA256.new(bytes(input.encode("utf8")))
+    # print("Hashed input in hex:", hash.hexdigest())
+
+def find_collision():
+    digest_dict = {}
+    #byte_form = int(input)
+    num = 5
+    while True:
+        d = num.to_bytes(length=(max(num.bit_length(),1) +7)//8, byteorder='big')
+        hash = SHA256.new(d)
+        digest = hash.hexdigest()
+        digest = collisions(digest)
+        if digest in digest_dict:
+            print("Collision found!")
+            print("Hash Digest: ", digest)
+            print("Integer: ", num)
+            return
+        else :
+            num +=1
+            digest_dict[digest] = num
+
+
+
 
 if __name__ == "__main__":
     data_1 = "10"
@@ -24,25 +46,22 @@ if __name__ == "__main__":
     collisions(data_1)
     collisions(data_2)
 
-    data_1 = 'Hellohello'
+    data_1 = '12345'
     data_2 = 'hellohello'
     
     print("--------------------------------------")
     collisions(data_1)
     collisions(data_2)
 
+    print("--------------------------------------")
+    find_collision()
 
 
 
 
-string1 = "Hello man"
-string1 = string1.encode('utf-8')
-h = SHA256.new(bytes(string1))
-print("Original digest: " ,  h.hexdigest())
-string2 = "Hell man"
-string2 = string2.encode('utf-8')
-h = SHA256.new(bytes(string2))
-print("New digest: " , h.hexdigest())
+
+
+
 
 
 
