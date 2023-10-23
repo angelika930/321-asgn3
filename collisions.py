a3549fd792c6a090c32f445af4554e1782f1f1d2
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-23 14:57:50 -0700

    prints string digest

--- collisions.py
+++ collisions.py
@@ -9,5 +9,17 @@ print("data:", data)
 h = SHA256.new(data)
 print("hex:", h.hexdigest())
 
+string1 = "Hello man"
+string1 = string1.encode('utf-8')
+h = SHA256.new(bytes(string1))
+print("Original digest: " ,  h.hexdigest())
+string2 = "Hell man"
+string2 = string2.encode('utf-8')
+h = SHA256.new(bytes(string2))
+print("New digest: " , h.hexdigest())
+
+
+
+
 
 
