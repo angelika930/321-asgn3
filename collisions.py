db7a98250df6ccc443e661c8399f1d56403b2ff2
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-23 16:59:50 -0700

    added time module

--- collisions.py
+++ collisions.py
@@ -2,6 +2,7 @@
 from minm_attack import *
 from random import randint
 from Crypto.Hash import SHA256
+import time
 
 def collisions(input: bytes):
     input = input[:8]
@@ -13,7 +14,8 @@ def collisions(input: bytes):
 def find_collision():
     digest_dict = {}
     #byte_form = int(input)
-    num = 5
+    seconds = time.time()
+    num = 13
     while True:
         d = num.to_bytes(length=(max(num.bit_length(),1) +7)//8, byteorder='big')
         hash = SHA256.new(d)
@@ -23,6 +25,7 @@ def find_collision():
             print("Collision found!")
             print("Hash Digest: ", digest)
             print("Integer: ", num)
+            print("Time: ", seconds)
             return
         else :
             num +=1
