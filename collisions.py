9f3cef1370893db3d3394aa37369538d7803b56a
Angelika Canete <52233077+angelika930@users.noreply.github.com>
2023-10-26 11:43:56 -0700

    updated find collisions file

--- collisions.py
+++ collisions.py
@@ -3,33 +3,33 @@ from minm_attack import *
 from random import randint
 from Crypto.Hash import SHA256
 import time
+import random
 
 def collisions(input: bytes):
     input = input[:8]
     return input
-    #print("Truncated input: ", input)
+    # print("Truncated input: ", input)
     # hash = SHA256.new(bytes(input.encode("utf8")))
     # print("Hashed input in hex:", hash.hexdigest())
 
-def find_collision():
-    digest_dict = {}
-    #byte_form = int(input)
-    seconds = time.time()
-    num = 13
+def find_collision(num):  #takes in a number of bits to randomly generate
+    di_dict = {}
+    seconds = time.thread_time()
+    count = 0
     while True:
-        d = num.to_bytes(length=(max(num.bit_length(),1) +7)//8, byteorder='big')
-        hash = SHA256.new(d)
+        data = str(random.getrandbits(num)).encode('utf-8')
+        hash = SHA256.new(data)
         digest = hash.hexdigest()
         digest = collisions(digest)
-        if digest in digest_dict:
+        if digest in di_dict:
             print("Collision found!")
             print("Hash Digest: ", digest)
-            print("Integer: ", num)
+            print("Count: ", count)
             print("Time: ", seconds)
             return
-        else :
-            num +=1
-            digest_dict[digest] = num
+        
+        di_dict[digest] = num
+        count +=1
 
 
 
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     collisions(data_2)
 
     print("--------------------------------------")
-    find_collision()
+    find_collision(4)
 
 
 
